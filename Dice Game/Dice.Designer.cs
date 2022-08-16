4b4f3c108fd5d0cb4b4cbad170189a29dd2c3920
Jason Hatfield <jasonmhatfield@pm.me>
2022-08-16 15:28:26 -0400

    Add project files.

---
+++ Dice Game/Dice.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Dice_Game
+{
+    partial class Dice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dice));
+            this.rollButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.dieOne6 = new System.Windows.Forms.PictureBox();
+            this.dieOne5 = new System.Windows.Forms.PictureBox();
+            this.dieOne4 = new System.Windows.Forms.PictureBox();
+            this.dieOne3 = new System.Windows.Forms.PictureBox();
+            this.dieOne2 = new System.Windows.Forms.PictureBox();
+            this.dieOne1 = new System.Windows.Forms.PictureBox();
+            this.dieTwo1 = new System.Windows.Forms.PictureBox();
+            this.dieTwo2 = new System.Windows.Forms.PictureBox();
+            this.dieTwo3 = new System.Windows.Forms.PictureBox();
+            this.dieTwo4 = new System.Windows.Forms.PictureBox();
+            this.dieTwo5 = new System.Windows.Forms.PictureBox();
+            this.dieTwo6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rollButton
+            // 
+            this.rollButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.rollButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rollButton.Location = new System.Drawing.Point(72, 129);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(93, 31);
+            this.rollButton.TabIndex = 0;
+            this.rollButton.Text = "Roll";
+            this.rollButton.UseVisualStyleBackColor = false;
+            this.rollButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 175);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 16);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // dieOne6
+            // 
+            this.dieOne6.Image = global::Dice_Game.Properties.Resources.DIE___6;
+            this.dieOne6.Location = new System.Drawing.Point(12, 12);
+            this.dieOne6.Name = "dieOne6";
+            this.dieOne6.Size = new System.Drawing.Size(100, 100);
+            this.dieOne6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieOne6.TabIndex = 9;
+            this.dieOne6.TabStop = false;
+            this.dieOne6.Visible = false;
+            // 
+            // dieOne5
+            // 
+            this.dieOne5.Image = global::Dice_Game.Properties.Resources.DIE___5;
+            this.dieOne5.Location = new System.Drawing.Point(12, 12);
+            this.dieOne5.Name = "dieOne5";
+            this.dieOne5.Size = new System.Drawing.Size(100, 100);
+            this.dieOne5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieOne5.TabIndex = 8;
+            this.dieOne5.TabStop = false;
+            this.dieOne5.Visible = false;
+            // 
+            // dieOne4
+            // 
+            this.dieOne4.Image = global::Dice_Game.Properties.Resources.DIE___4;
+            this.dieOne4.Location = new System.Drawing.Point(12, 12);
+            this.dieOne4.Name = "dieOne4";
+            this.dieOne4.Size = new System.Drawing.Size(100, 100);
+            this.dieOne4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieOne4.TabIndex = 7;
+            this.dieOne4.TabStop = false;
+            this.dieOne4.Visible = false;
+            // 
+            // dieOne3
+            // 
+            this.dieOne3.Image = global::Dice_Game.Properties.Resources.DIE___3;
+            this.dieOne3.Location = new System.Drawing.Point(12, 12);
+            this.dieOne3.Name = "dieOne3";
+            this.dieOne3.Size = new System.Drawing.Size(100, 100);
+            this.dieOne3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieOne3.TabIndex = 6;
+            this.dieOne3.TabStop = false;
+            this.dieOne3.Visible = false;
+            // 
+            // dieOne2
+            // 
+            this.dieOne2.Image = global::Dice_Game.Properties.Resources.DIE___2;
+            this.dieOne2.Location = new System.Drawing.Point(12, 12);
+            this.dieOne2.Name = "dieOne2";
+            this.dieOne2.Size = new System.Drawing.Size(100, 100);
+            this.dieOne2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieOne2.TabIndex = 5;
+            this.dieOne2.TabStop = false;
+            this.dieOne2.Visible = false;
+            // 
+            // dieOne1
+            // 
+            this.dieOne1.Image = global::Dice_Game.Properties.Resources.DIE___1;
+            this.dieOne1.Location = new System.Drawing.Point(12, 12);
+            this.dieOne1.Name = "dieOne1";
+            this.dieOne1.Size = new System.Drawing.Size(100, 100);
+            this.dieOne1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieOne1.TabIndex = 3;
+            this.dieOne1.TabStop = false;
+            // 
+            // dieTwo1
+            // 
+            this.dieTwo1.Image = global::Dice_Game.Properties.Resources.DIE___1;
+            this.dieTwo1.Location = new System.Drawing.Point(132, 12);
+            this.dieTwo1.Name = "dieTwo1";
+            this.dieTwo1.Size = new System.Drawing.Size(100, 100);
+            this.dieTwo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieTwo1.TabIndex = 10;
+            this.dieTwo1.TabStop = false;
+            this.dieTwo1.Visible = false;
+            // 
+            // dieTwo2
+            // 
+            this.dieTwo2.Image = global::Dice_Game.Properties.Resources.DIE___2;
+            this.dieTwo2.Location = new System.Drawing.Point(132, 12);
+            this.dieTwo2.Name = "dieTwo2";
+            this.dieTwo2.Size = new System.Drawing.Size(100, 100);
+            this.dieTwo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieTwo2.TabIndex = 11;
+            this.dieTwo2.TabStop = false;
+            // 
+            // dieTwo3
+            // 
+            this.dieTwo3.Image = global::Dice_Game.Properties.Resources.DIE___3;
+            this.dieTwo3.Location = new System.Drawing.Point(132, 12);
+            this.dieTwo3.Name = "dieTwo3";
+            this.dieTwo3.Size = new System.Drawing.Size(100, 100);
+            this.dieTwo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieTwo3.TabIndex = 12;
+            this.dieTwo3.TabStop = false;
+            this.dieTwo3.Visible = false;
+            // 
+            // dieTwo4
+            // 
+            this.dieTwo4.Image = global::Dice_Game.Properties.Resources.DIE___4;
+            this.dieTwo4.Location = new System.Drawing.Point(132, 12);
+            this.dieTwo4.Name = "dieTwo4";
+            this.dieTwo4.Size = new System.Drawing.Size(100, 100);
+            this.dieTwo4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieTwo4.TabIndex = 13;
+            this.dieTwo4.TabStop = false;
+            this.dieTwo4.Visible = false;
+            // 
+            // dieTwo5
+            // 
+            this.dieTwo5.Image = global::Dice_Game.Properties.Resources.DIE___5;
+            this.dieTwo5.Location = new System.Drawing.Point(132, 12);
+            this.dieTwo5.Name = "dieTwo5";
+            this.dieTwo5.Size = new System.Drawing.Size(100, 100);
+            this.dieTwo5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieTwo5.TabIndex = 14;
+            this.dieTwo5.TabStop = false;
+            this.dieTwo5.Visible = false;
+            // 
+            // dieTwo6
+            // 
+            this.dieTwo6.Image = global::Dice_Game.Properties.Resources.DIE___6;
+            this.dieTwo6.Location = new System.Drawing.Point(132, 12);
+            this.dieTwo6.Name = "dieTwo6";
+            this.dieTwo6.Size = new System.Drawing.Size(100, 100);
+            this.dieTwo6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dieTwo6.TabIndex = 15;
+            this.dieTwo6.TabStop = false;
+            this.dieTwo6.Visible = false;
+            // 
+            // Dice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(254, 204);
+            this.Controls.Add(this.dieTwo1);
+            this.Controls.Add(this.dieTwo2);
+            this.Controls.Add(this.dieTwo3);
+            this.Controls.Add(this.dieTwo4);
+            this.Controls.Add(this.dieTwo5);
+            this.Controls.Add(this.dieTwo6);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.rollButton);
+            this.Controls.Add(this.dieOne1);
+            this.Controls.Add(this.dieOne2);
+            this.Controls.Add(this.dieOne3);
+            this.Controls.Add(this.dieOne4);
+            this.Controls.Add(this.dieOne5);
+            this.Controls.Add(this.dieOne6);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Dice";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieOne1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieTwo6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox dieOne1;
+        private System.Windows.Forms.PictureBox dieOne2;
+        private System.Windows.Forms.PictureBox dieOne3;
+        private System.Windows.Forms.PictureBox dieOne4;
+        private System.Windows.Forms.PictureBox dieOne5;
+        private System.Windows.Forms.PictureBox dieOne6;
+        private System.Windows.Forms.PictureBox dieTwo1;
+        private System.Windows.Forms.PictureBox dieTwo2;
+        private System.Windows.Forms.PictureBox dieTwo3;
+        private System.Windows.Forms.PictureBox dieTwo4;
+        private System.Windows.Forms.PictureBox dieTwo5;
+        private System.Windows.Forms.PictureBox dieTwo6;
+    }
+}
+
---
+++ Dice Game/Dice.cs
@@ -0,0 +1,62 @@
+﻿// Jason Hatfield
+// This is my original work
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Dice_Game
+{
+    public partial class Dice : Form
+    {
+        public Dice()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        
+        private int numberOfSides = 6; // establishes the number of sides for each die
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rollDie();  // calls the rollDie() method.
+        }
+        /// <summary>
+        /// Method to roll two dice continuously until snake eyes is shown. Returns the count of rolls to get snake eyes.
+        /// </summary>
+        private void rollDie()
+        {
+            // clears the list box when the button is clicked.
+            int dieOne, dieTwo, count = 0;
+
+            do
+            {
+                dieOne = random.Next(1, numberOfSides + 1); // takes the number of sides and adds one to roll between 1 and that number.
+
+                dieTwo = random.Next(1, numberOfSides + 1); // takes the number of sides and adds one to roll between 1 and that number.
+                count++;
+
+            }
+            // while either the first or second die are not equal to one, the do loop repeats.
+            while (dieOne != 1 || dieTwo != 1);
+
+            // if statement to ensure the proper grammer is output if only one roll is needed to get to snake eyes.
+            if (count == 1)
+            {
+                outputLabel.Text = ("It took " + count + " roll to get snake eyes!");
+            }
+            else
+            {
+                outputLabel.Text = ("It took " + count + " rolls to get snake eyes!");
+            }
+        }
+    }
+}
